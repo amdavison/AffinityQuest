88fd1fc8acd963a4ce1214bece198d00c887e5f4
amdavison <andrewmdavison60@gmail.com>
2023-09-19 22:09:32 -0400

    add movement script for player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 2;
+
+    private void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    }
+}
