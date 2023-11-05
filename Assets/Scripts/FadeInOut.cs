3948fae950db883080ff65d59df1cf08826f0ac4
amdavison <andrewmdavison60@gmail.com>
2023-11-05 13:51:34 -0500

    add fade transition script

---
+++ Assets/Scripts/FadeInOut.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public bool fadein = false;
+    public bool fadeout = false;
+
+    public float timeToFade;
+
+    private void Update()
+    {
+        if (fadein == true)
+        {
+            if (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += timeToFade * Time.deltaTime;
+                if (canvasGroup.alpha >= 1)
+                {
+                    fadein = false;
+                }
+            }
+        }
+
+        if (fadeout == true)
+        {
+            if (canvasGroup.alpha >= 0)
+            {
+                canvasGroup.alpha -= timeToFade * Time.deltaTime;
+                if (canvasGroup.alpha == 0)
+                {
+                    fadeout = false;
+                }
+            }
+        }
+    }
+
+    public void FadeIn() { fadein = true; }
+
+    public void FadeOut() { fadeout = true; }
+}
