16221a24f69fe3bc5f3770e38af0130f1c5a9d2a
amdavison <andrewmdavison60@gmail.com>
2023-11-08 21:42:33 -0500

    add SFX call for button clicks

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -11,6 +11,7 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     public void StartQuest()
     {
+        AudioManager.instance.PlaySFX(AudioManager.instance.buttonClick);
         Debug.Log("Starting quest!");
         sceneChanger.LoadScene("GameScene");
     }
@@ -20,6 +21,7 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     public void QuitGame()
     {
+        AudioManager.instance.PlaySFX(AudioManager.instance.buttonClick);
         Debug.Log("Quitting application!");
         Application.Quit();
     }
